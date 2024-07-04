9ca909e4c65e9b15ddb8b2737f6243a3bee6cdc0
m.kuzhagulov <m.kuzhagulov@gmail.com>
2024-07-04 21:22:26 +0800

    Doobie + Http4s foundation

---
+++ src/main/scala/com/kuzhagulov/foundation/Doobie.scala
@@ -0,0 +1,83 @@
+package com.kuzhagulov.foundation
+
+import cats.effect.kernel.MonadCancelThrow
+import cats.effect.{IO, IOApp}
+import doobie.hikari.HikariTransactor
+import doobie.util.transactor.Transactor
+import doobie.implicits.*
+import doobie.util.ExecutionContexts
+
+object Doobie extends IOApp.Simple {
+
+  case class Student(id: Int, name: String)
+
+  val xa: Transactor[IO] = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",                  // JDBC Connector
+    "jdbc:postgresql://localhost:5432/demo",  // database URL
+    "docker",                                 // user
+    "docker"                                  // password
+  )
+
+  def findAllStudents: IO[List[String]] = {
+    val action = sql"SELECT name FROM students".query[String].to[List]
+    action.transact(xa)
+  }
+
+  def saveStudent(id: Int, name: String): IO[Int] = {
+    val action = sql"INSERT INTO students VALUES($id, $name)".update.run
+    action.transact(xa)
+  }
+
+  def findStudentsByInitial(letter: String): IO[List[Student]] = {
+    val selectPart = fr"SELECT id, name"
+    val fromPart = fr"FROM students"
+    val wherePart = fr"WHERE LEFT(name, 1) = $letter"
+
+    val wholeStatement = selectPart ++ fromPart ++ wherePart
+    val action = wholeStatement.query[Student].to[List]
+    action.transact(xa)
+  }
+
+  // Tagless Final approach
+
+  trait Students[F[_]] {
+    def findById(id: Int): F[Option[Student]]
+    def findAll: F[List[Student]]
+    def create(name: String): F[Int]
+  }
+
+  object Students {
+    def make[F[_]: MonadCancelThrow](xa: Transactor[F]) = new Students[F] {
+
+      override def findById(id: Int): F[Option[Student]] =
+        sql"SELECT id, name FROM students WHERE id = $id".query[Student].option.transact(xa)
+      override def findAll: F[List[Student]] =
+        sql"SELECT id, name FROM students".query[Student].to[List].transact(xa)
+      override def create(name: String): F[Int] =
+        sql"INSERT INTO students(name) values ($name)".update.withUniqueGeneratedKeys[Int]("id").transact(xa)
+    }
+  }
+
+  val postgresResource = for {
+    ec <- ExecutionContexts.fixedThreadPool[IO](16)
+    xa <- HikariTransactor.newHikariTransactor[IO](
+      "org.postgresql.Driver",
+      "jdbc:postgresql://localhost:5432/demo",
+      "docker",
+      "docker",
+      ec
+    )
+  } yield xa
+
+  val smallProgram = postgresResource.use { xa =>
+    val studentsRepo = Students.make[IO](xa)
+
+    for {
+      id <- studentsRepo.create("Marat")
+      marat <- studentsRepo.findById(id)
+      _ <- IO.println(s"The Senior Scala Developer of SBER: $marat")
+    } yield ()
+  }
+
+  override def run: IO[Unit] = smallProgram
+}
---
+++ src/main/scala/com/kuzhagulov/foundation/Http4s.scala
@@ -0,0 +1,97 @@
+package com.kuzhagulov.foundation
+
+import cats.*
+import cats.implicits.*
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+import org.http4s.circe.*
+import cats.effect.{IO, IOApp}
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.impl.{OptionalValidatingQueryParamDecoderMatcher, QueryParamDecoderMatcher}
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Router
+
+import java.util.UUID
+
+object Http4s extends IOApp.Simple {
+  type Student = String
+  case class Instructor(firstName: String, lastName: String)
+  case class Course(id: String, title: String, year: Int, students: List[Student], instructorName: String)
+
+  object CourseRepository {
+    val catsEffectCourse = Course(
+      "2976c2f3-e769-4edc-8fd1-e105c1a29cf2",
+      "Rock the JVM",
+      2024,
+      List("Marat", "Alice"),
+      "Martin Odersky"
+    )
+
+    private val courses: Map[String, Course] = Map(catsEffectCourse.id -> catsEffectCourse)
+
+    // API
+    def findCourseById(id: UUID): Option[Course] =
+      courses.get(id.toString)
+
+    def findCourseByInstructor(name: String): List[Course] = {
+      courses.values.filter(_.instructorName == name).toList
+    }
+  }
+
+  /*
+    REST Endpoints:
+    GET localhost:8080/courses?instructor=Martin%20Odersky&year=2024
+    GET localhost:8080/courses/2976c2f3-e769-4edc-8fd1-e105c1a29cf2/students
+  */
+
+  object InstructorQueryParameterMatcher extends QueryParamDecoderMatcher[String]("instructor")
+  object YearQueryParamMatcher extends OptionalValidatingQueryParamDecoderMatcher[Int]("year")
+
+  def courseRoutes[F[_]: Monad]: HttpRoutes[F] = {
+    val dsl = Http4sDsl[F]
+    import dsl.*
+
+    HttpRoutes.of[F] {
+      case GET -> Root / "courses" :? InstructorQueryParameterMatcher(instructor) +& YearQueryParamMatcher(maybeYear) =>
+        val courses = CourseRepository.findCourseByInstructor(instructor)
+        maybeYear match {
+          case Some(y) => y.fold(
+            _ => BadRequest("Parameter 'year' is invalid"),
+            year =>
+              val res = courses.filter(_.year == year)
+              Ok(res.asJson)
+          )
+          case None => Ok(courses.asJson)
+        }
+      case GET -> Root / "courses" / UUIDVar(courseId) / "students" =>
+        CourseRepository.findCourseById(courseId).map(_.students) match {
+          case Some(students) => Ok(students.asJson)
+          case None => NotFound(s"No course with $courseId was found.")
+        }
+    }
+  }
+
+  def healthEndpoint[F[_]: Monad]: HttpRoutes[F] = {
+    val dsl = Http4sDsl[F]
+    import dsl.*
+
+    HttpRoutes.of[F] {
+      case GET -> Root / "health" => Ok("All going great!")
+    }
+  }
+
+  def allRoutes[F[_]: Monad]: HttpRoutes[F] = courseRoutes[F] <+> healthEndpoint[F]
+
+  def routerWithPathPrefixes = Router(
+    "/api" -> courseRoutes[IO],
+    "/private" -> healthEndpoint[IO]
+  ).orNotFound
+
+  override def run: IO[Unit] =
+    EmberServerBuilder
+      .default[IO]
+      .withHttpApp(routerWithPathPrefixes)
+      .build
+      .use(_ => IO.println("Server started...") *> IO.never)
+}
